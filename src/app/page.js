096c017b0a22d14e1ee04dd90284af8d91d51b14
David Colgan <david@recursivegarden.com>
2023-09-03 22:07:13 +0200

    Move task data into an object and iterate

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,19 @@
 import clsx from "clsx";
 
-function Task({
-  important = false,
-  finished = false,
-  createdAt,
-  finishedAt,
-  children,
-}) {
+function Subtask({ title, finished = false }) {
+  return (
+    <div
+      className={clsx(
+        "p-1",
+        finished ? "text-slate-400 line-through" : "text-slate-900"
+      )}
+    >
+      {title}
+    </div>
+  );
+}
+
+function Task({ title, status, createdAt, finishedAt, subtasks = [] }) {
   return (
     <div className="border-2 border-slate-500 mb-1">
       {(createdAt || finishedAt) && (
@@ -18,12 +25,22 @@ function Task({
       <div
         className={clsx(
           "p-1",
-          finished ? "text-slate-400 line-through" : "text-slate-900",
-          important ? "text-3xl font-bold" : "text-base"
+          status === "finished"
+            ? "text-slate-400 line-through"
+            : "text-slate-900",
+          status === "most-important" ? "text-3xl font-bold" : "text-base"
         )}
       >
-        {children}
+        {title}
       </div>
+
+      {subtasks.map((subtask, index) => (
+        <Subtask
+          key={index}
+          title={subtask.title}
+          finished={subtask.finished}
+        />
+      ))}
     </div>
   );
 }
@@ -31,53 +48,110 @@ function Task({
 function TaskInput({}) {
   return (
     <>
-      <label className="italic text-slate-600 text-sm">Add New Task</label>
-      <textarea className="border-2 border-slate-400 p-2 w-full"></textarea>
+      <textarea
+        placeholder="Add New Task"
+        className="border-2 border-slate-400 p-2 w-full"
+      ></textarea>
     </>
   );
 }
 
 export default function Home() {
+  const tasks = [
+    {
+      id: 1,
+      status: "most-important",
+      title: "Allow adding a new task by typing it into a text box.",
+      subtasks: [
+        { finished: true, title: "Create a new component for task input" },
+        {
+          finished: false,
+          title:
+            "Pull out the data from being hardcoded and put it into a data structure.",
+        },
+        { finished: false, title: "Make the input widget into a form" },
+        { finished: false, title: "Create a submit button" },
+        { finished: false, title: "Persist the data to some kind of storage." },
+      ],
+    },
+    {
+      id: 2,
+      status: "backlog",
+      title: "Format task timestamps in a human-readable way.",
+    },
+    {
+      id: 3,
+      status: "finished",
+      title: "Allow creating subtasks for complex tasks.",
+    },
+    {
+      id: 4,
+      status: "finished",
+      title:
+        "Allow specifying the datetime stamps of when the task is first created and when it is completed.",
+    },
+    {
+      id: 5,
+      status: "finished",
+      title:
+        "Make the next most important thing visibly different from other tasks",
+    },
+    { id: 6, status: "finished", title: "Allow marking a task done." },
+    { id: 7, status: "finished", title: "Allow adding a new task." },
+    {
+      id: 8,
+      status: "finished",
+      title:
+        "Allow specifying the next most important task, and put it at the top of the screen.",
+    },
+  ];
+
   return (
     <main>
       <h3 className="text-xl mb-4">Most Important Task</h3>
-      <Task important={true}>
-        Allow adding a new task by typing it into a text box.
-        <Task finished={true}>Create a new component for task input</Task>
-        <Task>Create a submit button</Task>
-        <Task>Make the input widget into a form</Task>
-        <Task>
-          Pull out the data from being hardcoded and put it into a data
-          structure.
-        </Task>
-        <Task>Persist the data to some kind of storage.</Task>
-        <TaskInput />
-      </Task>
+      {tasks
+        .filter((task) => task.status === "most-important")
+        .map((task) => (
+          <>
+            <Task
+              key={task.id}
+              title={task.title}
+              status={task.status}
+              subtasks={task.subtasks}
+            />
+          </>
+        ))}
 
       <TaskInput />
       <h3 className="text-3xl mb-4">Backlog</h3>
-      <Task>Format task timestamps in a human-readable way.</Task>
+
+      {tasks
+        .filter((task) => task.status === "backlog")
+        .map((task) => (
+          <>
+            <Task
+              key={task.id}
+              title={task.title}
+              status={task.status}
+              subtasks={task.subtasks}
+            />
+          </>
+        ))}
+
       <h3 className="text-3xl mb-4">Finished</h3>
 
-      <Task finished={true}>Allow creating subtasks for complex tasks.</Task>
-
-      <Task
-        createdAt={"2023-09-03T18:50:49.843Z"}
-        finishedAt={"2023-09-03T18:51:14.226Z"}
-        finished={true}
-      >
-        Allow specifying the datetime stamps of when the task is first created
-        and when it is completed.
-      </Task>
-      <Task finished="true">
-        Make the next most important thing visibly different from other tasks
-      </Task>
-      <Task finished={true}>Allow marking a task done.</Task>
-      <Task finished={true}>Allow adding a new task.</Task>
-      <Task finished={true}>
-        Allow specifying the next most important task, and put it at the top of
-        the screen.
-      </Task>
+      {tasks
+        .filter((task) => task.status === "finished")
+        .map((task) => (
+          <>
+            <Task
+              key={task.id}
+              title={task.title}
+              status={task.status}
+              subtasks={task.subtasks}
+            />
+          </>
+        ))}
     </main>
   );
 }
