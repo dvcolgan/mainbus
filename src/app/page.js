6da5e348db9ee2f9b235d4bd054782ec914d5ca2
David Colgan <david@recursivegarden.com>
2023-09-03 21:24:18 +0200

    Task input box and subtasks

--- src/app/page.js
+++ src/app/page.js
@@ -8,16 +8,18 @@ function Task({
   children,
 }) {
   return (
-    <div className="border-4 border-slate-500 mb-4">
-      <div className="bg-slate-300">
-        {createdAt && <span>Created: {createdAt}</span>}&nbsp;
-        {finishedAt && <span>Finished: {finishedAt}</span>}
-      </div>
+    <div className="border-2 border-slate-500 mb-1">
+      {(createdAt || finishedAt) && (
+        <div className="bg-slate-300">
+          {createdAt && <span>Created: {createdAt}</span>}&nbsp;
+          {finishedAt && <span>Finished: {finishedAt}</span>}
+        </div>
+      )}
       <div
         className={clsx(
-          "font-bold p-4",
+          "p-1",
           finished ? "text-slate-400 line-through" : "text-slate-900",
-          important ? "text-3xl" : "text-xl"
+          important ? "text-3xl font-bold" : "text-base"
         )}
       >
         {children}
@@ -26,22 +28,43 @@ function Task({
   );
 }
 
+function TaskInput({}) {
+  return (
+    <>
+      <label className="italic text-slate-600 text-sm">Add New Task</label>
+      <textarea className="border-2 border-slate-400 p-2 w-full"></textarea>
+    </>
+  );
+}
+
 export default function Home() {
   return (
     <main>
-      <h3 className="text-3xl mb-4">Most Important Task</h3>
+      <h3 className="text-xl mb-4">Most Important Task</h3>
       <Task important={true}>
         Allow adding a new task by typing it into a text box.
+        <Task finished={true}>Create a new component for task input</Task>
+        <Task>Create a submit button</Task>
+        <Task>Make the input widget into a form</Task>
+        <Task>
+          Pull out the data from being hardcoded and put it into a data
+          structure.
+        </Task>
+        <Task>Persist the data to some kind of storage.</Task>
+        <TaskInput />
       </Task>
+
+      <TaskInput />
       <h3 className="text-3xl mb-4">Backlog</h3>
       <Task>Format task timestamps in a human-readable way.</Task>
       <h3 className="text-3xl mb-4">Finished</h3>
+
+      <Task finished={true}>Allow creating subtasks for complex tasks.</Task>
 
       <Task
         createdAt={"2023-09-03T18:50:49.843Z"}
         finishedAt={"2023-09-03T18:51:14.226Z"}
         finished={true}
-        important={true}
       >
         Allow specifying the datetime stamps of when the task is first created
         and when it is completed.
